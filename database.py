3d82fce4f6677dab2eae286d68a2c91746212da8
dotdm26 <thaiducminhdo@gmail.com>
2024-12-07 18:04:21 +0000

    Added more comments in database.py for better reading

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 load_dotenv()
 
-# Setup MongoDB. Keys are stored in an .env file
+# Setup MongoDB. Keys are stored locally in an .env file to avoid unwanted access
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
@@ -21,8 +21,7 @@ locations = db['locations']
 search_history = db['search_history']
 
 #check if city has been searched first, then update time.
-#if not, add city to database
-#TODO: Ensure city name in the database is saved with the first letter capitalized
+#if not, add city to "location" database
 def find_location(location, address, lat, lon):
     #find by lat & long
     result = locations.find_one({"address": address})
@@ -38,17 +37,19 @@ def find_location(location, address, lat, lon):
                               "searches": 1})
     add_to_history(location.capitalize())
         
-#add to search_history collection for tracking recent searches.
+#add to "search_history" collection for tracking recent searches.
 def add_to_history(location):
     search_history.insert_one({"location": location,
                                "timestamp": datetime.now()})
 
+#display latest search results (retrieve data from "search_history" collection)
 def show_latest_searches():
     results = list(search_history.find().sort("timestamp", -1).limit(5))
     search_result = [result['location'] for result in results]
 
     return jsonify({"locations": search_result})
 
+#display most searched results (in "locations" collection in mongodb)
 def show_most_searches():
     results = list(locations.find().sort("searches", -1).limit(5))
     address_name = [result['address'] for result in results]
